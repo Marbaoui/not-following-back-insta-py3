30cc354607bb3f722e50f4d65cc8f6aab5c9ad7b
Marbaoui <arb.med241@gmail.com>
2022-08-07 02:21:25 +0200

    Update NotFollowingBack.py

--- NotFollowingBack.py
+++ NotFollowingBack.py
@@ -5,19 +5,29 @@ def main():
     targetProfile = "The target profie"
     Username = "Your username"
     Pass = "Your password"
-    L = Instaloader()
-    profile = Profile.from_username(L.context, targetProfile)
-    L.login(Username, Pass)
-    def intersection(lst1, lst2):
-        return list(set(lst1) & set(lst2))
+ 
+from instaloader import Instaloader, Profile
+import os
 
-    following = set(profile.get_followees())
-    followers = set(profile.get_followers())
 
-    notFollowers= intersection((following^followers), following)
+known_account =  [] # contains know insta account to not include in comparison
 
-    notFollowersUsername = (map(lambda user: user.username, notFollowers))
+print("loading...")
+targetProfile = "The target profie"
+Username = "Your username"
+Pass = os.getenv("passinsta")
+L = Instaloader()
+profile = Profile.from_username(L.context, targetProfile)
+L.login(Username, Pass)
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
 
-    print(list(notFollowersUsername))
+following = set(profile.get_followees())
+followers = set(profile.get_followers())
 
-main()
+
+notFollowers= intersection((following^followers), following)
+
+notFollowersUsername = list(map(lambda user: user.username, notFollowers))
+result = list(filter(lambda x: x not in known_account, notFollowersUsername))
+print(result)
